c53fc469b5ba3be229f971204c10e90d8d5168d4
shng5175 <shng5175@colorado.edu>
2018-04-10 15:53:55 -0600

    Testing things

---
+++ test.cpp
@@ -0,0 +1,20 @@
+#define CATCH_CONFIG_MAIN 
+
+#include "catch.h"
+#include "Math_utils.h"
+
+TEST_CASE("Squares are computed", "[isSquare]") {
+    Math thing;
+    REQUIRE(thing.isSquare(4) == true);
+    REQUIRE(thing.isSquare(25) == true);
+    REQUIRE(thing.isSquare(5) == false);
+    REQUIRE(thing.isSquare(36) == true);
+    REQUIRE(thing.isSquare(101) == false);
+}
+
+TEST_CASE("Gets correct digit", "[GetDigit]") {
+    Math thing;
+    REQUIRE(thing.GetDigit(234, 1) == 4);
+    REQUIRE(thing.GetDigit(234, 10) == 3);
+    REQUIRE(thing.GetDigit(234, 100) == 2);
+}
