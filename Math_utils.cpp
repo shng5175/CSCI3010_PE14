6ac362fc17c8ae40f347847a6e4a90d77b8e49e4
shng5175 <shng5175@colorado.edu>
2018-04-10 15:42:54 -0600

    More things

--- Math_utils.cpp
+++ Math_utils.cpp
@@ -1,5 +1,8 @@
 #include "Math_utils.h"
 #include <math.h>
+#include <iostream>
+
+using namespace std;
 
 bool Math::isSquare(int num) {
     double root = sqrt(num);
@@ -11,14 +14,47 @@ bool Math::isSquare(int num) {
 }
 
 int Math::GetDigit(int num, int place) {
+    for (; num > 0; num /= 10) digits.push_back(num % 10);
 
-    return 0;
+    /*for (int i = 0; i < digits.size(); i++) {
+        cout << digits[i] << endl;
+    }
+    cout << digits.size()<< endl;*/
+
+    int pl = 0;
+    while (place > 1) {
+        place = place / 10;
+        pl++;
+    }
+
+    if (place < 0 || pl >= digits.size()) {
+        cout << "Error!!" << endl;
+        return false;
+    }
+    else {
+        int val = digits[pl];
+        return val;
+    }
 }
 
 bool Math::EqualParity(int x, int y) {
-    return 0;
+    if (((x < 0) && (y < 0)) || ((x > 0) && (y > 0)))
+        return true;
+    else
+        return false;
 }
 
 bool Math::EqualParity(std::vector<int> nums) {
-    return 0;
+    int is_pos = 0;
+    int is_neg = 0;
+    for (int i = 0; i < nums.size(); i++) {
+        if (nums[i] < 0)
+            is_neg++;
+        else
+            is_pos++;
+    }
+    if (is_pos == nums.size() || is_neg == nums.size())
+        return true;
+    else
+        return false;
 }
