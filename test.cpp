cbd96e79c5bb5a45753d81ef7a90770b49801905
Devon Soto <deso8093@colorado.edu>
2018-04-10 21:01:26 -0600

    correct test/catch file, added test cases

--- test.cpp
+++ test.cpp
@@ -1,20 +1,56 @@
 #define CATCH_CONFIG_MAIN 
 
-#include "catch.h"
+#include "catch.hpp"
 #include "Math_utils.h"
 
 TEST_CASE("Squares are computed", "[isSquare]") {
     Math thing;
+
+    // squares should return true
     REQUIRE(thing.isSquare(4) == true);
     REQUIRE(thing.isSquare(25) == true);
-    REQUIRE(thing.isSquare(5) == false);
     REQUIRE(thing.isSquare(36) == true);
+    REQUIRE(thing.isSquare(0) == true);
+
+
+    // non squares
+    REQUIRE(thing.isSquare(5) == false);
     REQUIRE(thing.isSquare(101) == false);
+    REQUIRE(thing.isSquare(-4) == false);
 }
 
-TEST_CASE("Gets correct digit", "[GetDigit]") {
-    Math thing;
-    REQUIRE(thing.GetDigit(234, 1) == 4);
-    REQUIRE(thing.GetDigit(234, 10) == 3);
-    REQUIRE(thing.GetDigit(234, 100) == 2);
+
+
+TEST_CASE("Gets correct digit", "[GetDigit]") {    
+    SECTION("Ones place") {
+        Math ones;
+        REQUIRE(ones.GetDigit(234, 1) == 4);
+    }
+
+    SECTION("Tens place") {
+        Math tens; 
+        REQUIRE(tens.GetDigit(234, 10) == 3);
+    }
+
+    SECTION("One hundreds place") {
+        Math hundereds;
+        REQUIRE(hundereds.GetDigit(123, 100) == 1);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
